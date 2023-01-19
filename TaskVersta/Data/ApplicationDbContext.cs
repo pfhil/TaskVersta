d3dcd1956695b93c2458bbec8d6f5abf4bc4873d
Sklyarenko Philipp <filipp.sklyarenko@mail.ru>
2023-01-19 13:34:32 +0300

    Создал миграцию БД с таблицей заказов

--- TaskVersta/Data/ApplicationDbContext.cs
+++ TaskVersta/Data/ApplicationDbContext.cs
@@ -5,6 +5,11 @@ namespace TaskVersta.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
         public DbSet<Order> Orders { get; set; }
     }
 }

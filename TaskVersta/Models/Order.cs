47ef9bd22e89f41e9198ff5fd49a17fbe7d388b4
Sklyarenko Philipp <filipp.sklyarenko@mail.ru>
2023-01-20 02:35:52 +0300

    Добавил аннотации данных для текста ошибок валидации модели Order

--- TaskVersta/Models/Order.cs
+++ TaskVersta/Models/Order.cs
@@ -5,24 +5,31 @@ namespace TaskVersta.Models
     public class Order
     {
         [Display(Name = "Номер заказа")]
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
         public int Id { get; set; }
 
         [Display(Name = "Город отправителя")]
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
         public string SenderCity { get; set; }
 
         [Display(Name = "Адрес отправителя")]
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
         public string SenderAddress { get; set; }
 
         [Display(Name = "Город получателя")]
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
         public string RecipientCity { get; set; }
 
         [Display(Name = "Адрес получателя")]
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
         public string RecipientAddress { get; set; }
 
         [Display(Name = "Вес груза")]
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
         public double CargoWeight { get; set; }
 
         [Display(Name = "Дата забора груза")]
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
         [DataType(DataType.Date)]
         public DateOnly DateCargoPickup { get; set; }
     }

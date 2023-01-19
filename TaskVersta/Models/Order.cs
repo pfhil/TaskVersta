1ee458c81894a12127ac0f64ab9f4fd8bc5c041c
Sklyarenko Philipp <filipp.sklyarenko@mail.ru>
2023-01-20 01:45:15 +0300

    Добавил аннтации данных для вывода имен полей, добавил аннтацию данных использования DatePicker на поле DateCargoPickup

--- TaskVersta/Models/Order.cs
+++ TaskVersta/Models/Order.cs
@@ -1,19 +1,29 @@
-﻿namespace TaskVersta.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskVersta.Models
 {
     public class Order
     {
+        [Display(Name = "Номер заказа")]
         public int Id { get; set; }
 
+        [Display(Name = "Город отправителя")]
         public string SenderCity { get; set; }
 
+        [Display(Name = "Адрес отправителя")]
         public string SenderAddress { get; set; }
 
+        [Display(Name = "Город получателя")]
         public string RecipientCity { get; set; }
 
+        [Display(Name = "Адрес получателя")]
         public string RecipientAddress { get; set; }
 
+        [Display(Name = "Вес груза")]
         public double CargoWeight { get; set; }
 
+        [Display(Name = "Дата забора груза")]
+        [DataType(DataType.Date)]
         public DateOnly DateCargoPickup { get; set; }
     }
 }
